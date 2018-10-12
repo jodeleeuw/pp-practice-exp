d46f458a9c871106c1d25e8f0d95ae9e5a62db12
Josh de Leeuw <josh.deleeuw@gmail.com>
2018-10-12 14:34:34 -0400

    use 6.1 instead

--- custom_plugins/jspsych-memory-quiz.js
+++ custom_plugins/jspsych-memory-quiz.js
@@ -111,7 +111,7 @@ jsPsych.plugins["memory-quiz"] = (function() {
       jsPsych.pluginAPI.getKeyboardResponse({
         callback_function: after_response,
         valid_responses: [13],
-        rt_method: 'date',
+        rt_method: 'performance',
         persist: false,
         allow_held_key: false
       });
